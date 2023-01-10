6f48155ada71e6f17e1004455cde44cd321b4cab
Justin Schreiber <jschreiber234@gmail.com>
2023-01-10 02:21:26 -0500

    add github config

--- forge.config.js
+++ forge.config.js
@@ -5,74 +5,59 @@ const os = require('os');
 let iconFile;
 let platform = os.platform();
 
-const iconFileWindows = path.resolve(__dirname,  "src/icons/win/icon.ico");
-const installerGifWindows = path.resolve(__dirname,  "src/icons/win/installerGif.gif");
+const iconFileWindows = path.resolve(__dirname, "src/icons/win/icon.ico");
+const installerGifWindows = path.resolve(__dirname, "src/icons/win/installerGif.gif");
 const cpDate = "Copyright (c) 2016, MIT";
 
-const iconFileMac = path.resolve(__dirname,  "src/icons/mac/icon.icns");
-const iconFileLinux = path.resolve(__dirname,  "src/icons/png/512x512.png");
+const iconFileMac = path.resolve(__dirname, "src/icons/mac/icon.icns");
+const iconFileLinux = path.resolve(__dirname, "src/icons/png/512x512.png");
 if (platform === 'darwin') {
-    iconFile = iconFileMac;
+  iconFile = iconFileMac;
 }
 else if (platform === 'win32') {
-     iconFile =   iconFileWindows;
+  iconFile = iconFileWindows;
 }
 else if (platform === 'linux') {
-	iconFile = iconFileLinux;
+  iconFile = iconFileLinux;
 }
 
 module.exports = {
-	"packagerConfig": {
-        "icon": iconFile,
-		appCopyright: "Copyright (c) 2016, MIT"
-      },
-	"makers": [
-        {
-          "name": "@electron-forge/maker-squirrel",
-          "config": {
-            "name": "ScratchJr"
-          }
-        },
-        {
-          "name": "@electron-forge/maker-zip",
-          "platforms": [
-            "darwin",
-            "win32"
-          ]
-        },
-        {
-          "name": "@electron-forge/maker-deb",
-          "config": {
-            "options": {
-              "icon": iconFile
-            }
-          }
-        },
-        {
-          "name": "@electron-forge/maker-rpm",
-          "config": {
-            "options": {
-              "icon": iconFile
-            }
-          }
-        },
-        {
-          "name": "@reforged/maker-appimage",
-          "config": {
-            "options": {
-              "name": "ScratchJr",
-			  "bin": "ScratchJr",
-              "productName": "ScratchJr",
-              "genericName": "ScratchJr",
-              "icon": iconFile,
-              "categories": [
-                "Education"
-              ],
-              "AppImageKitRelease": "continuous"
-            }
-          }
-        }
+  "packagerConfig": {
+    "icon": iconFile,
+    appCopyright: "Copyright (c) 2016, MIT"
+  },
+  "makers": [
+    {
+      "name": "@electron-forge/maker-squirrel",
+      "config": {
+        "name": "ScratchJr"
+      }
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "darwin",
+        "win32",
+        "linux"
       ]
+    },
+    ,
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": {
+        "options": {
+          "icon": iconFile
+        }
+      }
+    },
+    {
+      "name": "@electron-forge/maker-rpm",
+      "config": {
+        "options": {
+          "icon": iconFile
+        }
+      }
+    }
+  ]
 
 }
-  
